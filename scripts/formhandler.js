e234edb24ef6451fadf7cfa4bcb4431dd3ad9009
smaran39 <smaran3939@gmail.com>
2017-03-20 00:37:02 -0700

    after finishing chapter 12,challenge pending

--- scripts/formhandler.js
+++ scripts/formhandler.js
@@ -30,6 +30,21 @@
         });
     };
 
+    FormHandler.prototype.addInputHandler = function(fn) {
+        console.log('Setting input handler for form');
+        this.$formElement.on('input', '[name="emailAddress"]', function(event) {
+            var emailAddress = event.target.value;
+            //console.log(fn(emailAddress));
+            var message = '';
+            if (fn(emailAddress)) {
+                event.target.setCustomValidity('');
+            } else {
+                message = emailAddress + ' is not an authorized email address!';
+                event.target.setCustomValidity(message);
+            }
+        });
+    };
+
     App.FormHandler = FormHandler;
     window.App = App;
 })(window);
